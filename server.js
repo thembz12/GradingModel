cd897a69948b66671f711ef9e4b1255ab6245210
thembz12 <oyin1239@gmail.com>
2024-08-09 15:51:50 +0100

    update 2

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ require("./config/dbConfig")
 const port = process.env.port || 4321
 const app = express()
 
-
+const cors = require(`cors`)
 
 
 app.use(express.json())
+app.use(cors("*"))
 app.use("/api/v1/", router)
 app.use("/api/v1/", studentrouter)
 
